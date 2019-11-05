6c2f9c461be6ac3838794b88c7634265902719be
LightMingMing <1849901944@qq.com>
2019-11-05 23:32:37 +0800

    Display message in boot

---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+bin=bin
+matcha_img=${bin}/matcha.img
+boot_bin=${bin}/boot.bin
+
+bochs_config=${bin}/bochs_config
+
+# make
+cd src/boot && make && cd ../..
+
+# create when image file does not exist
+if [[ ! -e ${matcha_img} ]]; then
+	bximage -mode=create -fd=1.44M ${matcha_img} -q
+fi
+
+dd if=${boot_bin} of=${matcha_img} bs=512 count=1 conv=notrunc >> /dev/null 2>&1
+
+if [[ ! -e ${bochs_config} ]]; then
+	cat>${bochs_config}<<EOF
+romimage: file=/usr/local/share/bochs/BIOS-bochs-latest
+vgaromimage: file=/usr/local/share/bochs/VGABIOS-lgpl-latest
+floppya: 1_44=${matcha_img}, status=inserted
+boot: floppy
+EOF
+fi
+
+bochs -f ${bochs_config} -q
