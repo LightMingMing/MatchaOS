891b26a77d3aa8a1126afe0d5bc3b28bf9cb40fb
LightMingMing <1849901944@qq.com>
2019-11-09 20:39:58 +0800

    Load 'loader.bin' to memory

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,7 +1,9 @@
 ; BIOS加电自检后, 跳转至0x7c00执行boot引导程序
 org	0x7c00
 
-BaseOfStack equ	0x7c00
+BaseOfStack	equ 0x7c00
+BaseOfLoader	equ 0x1000
+OffsetOfLoader	equ 0x00
 
 %include "fat12.inc"
 
@@ -15,62 +17,64 @@ Label_Start:
 
 	; 清屏
 	mov	ax, 0600h
+	mov	bx, 0700h
 	mov	cx, 0
 	mov	dx, 0184fh
 	int	10h
 
 	; 设置光标位置
-	mov	ax, 0200h
-	mov	bx, 0
-	mov	dx, 0
-	int	10h
+	;mov	ax, 0200h
+	;mov	bx, 0
+	;mov	dx, 0
+	;int	10h
 	
-
 	; 显示"Start Boot"
-	mov	ax, 1301h
-	mov	bx, 000fh
 	mov	cx, 10
-	mov	dx, 0000h
-	push	ax
-	mov	ax, ds
-	mov	es, ax
-	pop	ax
-	mov	bp, StartBootMessage	; es:bp指向要显示的字符串的地址
-	int	10h
-	inc	byte [DisMsgLineNum]
+	mov	bp, StartBootMessage
+	call	Func_Display_Message
 
-	; test read sector
-	mov	ax, 0x9000
-	mov	es, ax
-	mov	ax, 0
-	mov	bx, 0
+	; 搜索 "Loader.bin"
+	mov	ax, LoaderFileName
+	call	Func_Search_File_In_Root_Dir
+	cmp	ax, 0
+	je	Label_No_LoaderBin
+
+	; 将Loader.bin加载至内存0x10000处	
+	mov	bx, BaseOfLoader
+	mov	es, bx
+	mov	bx, OffsetOfLoader
+	
+Label_Load_File:
+	push	ax
+	push	bx
+	mov	ah, 0eh
+	mov	al, '.'
+	mov	bl, 0fh
+	int	10h
+	pop	bx
+	pop	ax	
+	
+	push	ax
+	add	ax, FirstDataSecNum
+	sub	ax, FirstDataClus
 	mov	cl, 1
 	call	Func_Read_Sector
+	pop	ax	
+	call	Func_Next_Cluster_In_FAT_Entry
+	cmp	ax, 0fffh
+	jz	Label_File_Loaded
+	add	bx, [BPB_BytsPerSec]
+	jmp	Label_Load_File
 
-	; search "Loader.bin"
-	mov	si, LoaderFileName
-	call	Func_Search_File_In_Root_Dir
-	cmp	cx, 0
-	je	Label_No_LoaderBin
-	
-	jmp	Label_End
+Label_File_Loaded:
+	; 跳转至loader
+	jmp	BaseOfLoader:OffsetOfLoader
 
 Label_No_LoaderBin:
-	mov     ax, 1301h
-	mov     bx, 000ch
-	mov     cx, 15
-	mov     dh, byte [DisMsgLineNum]
-	mov     dl, 0
-	push    ax
-	mov     ax, ds
-	mov     es, ax
-	pop     ax
-	mov     bp, NoLoaderErr
-	int     10h
-
-Label_End:
-	jmp Label_Finish
-
+;	mov     cx, 15
+;	mov	bp, NoLoaderErr
+;	call	Func_Display_Error_Message
+	jmp	Label_Finish
 
 StartBootMessage:	db "Start Boot"
 NoLoaderErr:		db "No LOADER Found"
--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -2,17 +2,30 @@ org 10000h
 
 %include "fat12.inc"
 
+jmp	Label_Start
+
+; 多个扇区加载测试, loader文件大小大于10个扇区
+times	10240 db 0
+
 Label_Start:
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	mov	ax, 0
+	mov	ss, ax
+	mov	sp, 0x7c00
+
 	mov	ax, 1301h
 	mov	bx, 000fh
 	mov	cx, 12
-	mov	dx, 0100h
+	mov	dx, 0200h
 	push	ax,
 	mov	ax, ds
 	mov	es, ax
 	pop	ax
 	mov	bp, StartLoaderMessage
-	
+	int	10h
+
 	jmp	Label_Finish
 
-StartLoaderMessage:	db "Loader Start"
+StartLoaderMessage:	db "Start Loader"
