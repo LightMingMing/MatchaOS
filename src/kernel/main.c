504c2db27213045eb9fb33f42de17d2007bebe60
LightMingMing <1849901944@qq.com>
2019-11-18 22:44:03 +0800

    Display color on screen

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,37 @@
-void Start_Kernel(void)
+//
+// Created by 赵明明 on 2019/11/18.
+//
+
+void Start_Kernel()
 {
-	while(1);
+    // Linear Address of Frame Buffer
+    int *address = (int *) 0xffff800000a00000;
+    int i;
+
+    for (i = 0; i < 1440 * 16; i++)
+    {
+        *((char *) address + 0) = (char) 0xff;
+        *((char *) address + 1) = (char) 0x00;
+        *((char *) address + 2) = (char) 0x00;
+        *((char *) address + 3) = (char) 0x00;
+        address++;
+    }
+    for (i = 0; i < 1440 * 16; i++)
+    {
+        *((char *) address + 0) = (char) 0x00;
+        *((char *) address + 1) = (char) 0xff;
+        *((char *) address + 2) = (char) 0x00;
+        *((char *) address + 3) = (char) 0x00;
+        address++;
+    }
+    for (i = 0; i < 1440 * 16; i++)
+    {
+        *((char *) address + 0) = (char) 0x00;
+        *((char *) address + 1) = (char) 0x00;
+        *((char *) address + 2) = (char) 0xff;
+        *((char *) address + 3) = (char) 0x00;
+        address++;
+    }
+
+    __asm__ __volatile__ ("hlt":: :);
 }
