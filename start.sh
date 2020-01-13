337902f7e87701cd55d8f6682f344e9d3473537c
LightMingMing <1849901944@qq.com>
2020-01-13 23:06:11 +0800

    Polishing Makefile

--- start.sh
+++ start.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 bin=bin
 matcha_img=${bin}/matcha.img
-boot_bin=${bin}/boot.bin
-loader_bin=${bin}/loader.bin
-kernel_bin=${bin}/kernel.bin
-
 bochs_config=${bin}/bochs_config
-
-# make
-cd src/boot && make && cd ../..
-
-# create when image file does not exist
-if [[ ! -e ${matcha_img} ]]; then
-	bximage -mode=create -fd=1.44M ${matcha_img} -q
-fi
-
-dd if=${boot_bin} of=${matcha_img} bs=512 count=1 conv=notrunc >> /dev/null 2>&1
-
-# mount 'loader.bin' for mac
-mount_node=$(hdiutil mount ${matcha_img} | sed 's/^[^[:space:]]*[[:space:]]*//')
-echo "${mount_node}"
-cp ${loader_bin} "${mount_node}"
-cp ${kernel_bin} "${mount_node}"
-umount "${mount_node}"
 
 if [[ ! -e ${bochs_config} ]]; then
 	cat>${bochs_config}<<EOF
