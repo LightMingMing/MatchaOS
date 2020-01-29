43eaaf6eaa83bf153eae1ad577d1bc26bc12d404
LightMingMing <1849901944@qq.com>
2020-01-29 20:39:04 +0800

    Submit disk request manually

--- src/kernel/driver/disk.c
+++ src/kernel/driver/disk.c
@@ -5,6 +5,7 @@
 #include "../trap/apic.h"
 #include "../lib/x86.h"
 #include "../lib/stdio.h"
+#include "../mm/slab.h"
 
 irq_ctl_t disk_ctl = {
         .enable = io_apic_enable,
@@ -13,23 +14,99 @@ irq_ctl_t disk_ctl = {
         .uninstall = io_apic_uninstall,
         .ack = io_apic_edge_ack
 };
+struct block_buffer_node block_request;
+struct block_buffer_node *in_using = NULL;
+uint32_t req_cnt;
+
+void destroy_request();
+
+void exec_next_cmd() {
+    struct block_buffer_node *node = NULL;
+    struct ide_device *dev = NULL;
+
+    node = in_using = block_next(&block_request);
+    dev = node->dev;
+    list_del(&node->list);
+    req_cnt--;
+
+    int ext = node->LBA > 0xFFFFFFFU; // 28 bit PIO or 48 bit PIO
+
+    io_out8(dev->base + REG_DEV_SEL, 0xE0U | (dev->drive & 0x1U) << 4U |
+                                     (ext ? 0U : ((node->LBA >> 24U) & 0xFU)));
+
+    io_in8(dev->ctrl + REG_ALT_STATUS); // 400ns delays
+    io_in8(dev->ctrl + REG_ALT_STATUS);
+    io_in8(dev->ctrl + REG_ALT_STATUS);
+    io_in8(dev->ctrl + REG_ALT_STATUS);
+
+    uint8_t status = io_in8(dev->base + REG_STATUS);
+    if (status == 0) {
+        print_color(RED, BLACK, "Device does not exist\n");
+        destroy_request();
+        return;
+    }
+    // wait busy clear and data rdy
+    while (status & STATUS_BSY || !(status & STATUS_RDY)) {
+        status = io_in8(dev->base + REG_STATUS);
+    }
+
+    if (ext) {
+        io_out8(dev->base + REG_SEC_CNT, node->sec_cnt >> 8U); // sector count
+        io_out8(dev->base + REG_LBA_HIGH, node->LBA >> 40U);
+        io_out8(dev->base + REG_LBA_MED, node->LBA >> 32U);
+        io_out8(dev->base + REG_LBA_LOW, node->LBA >> 24U);
+    }
+
+    io_out8(dev->base + REG_SEC_CNT, node->sec_cnt);
+    io_out8(dev->base + REG_LBA_HIGH, (node->LBA >> 16U) & 0xFFU);
+    io_out8(dev->base + REG_LBA_MED, (node->LBA >> 8U) & 0xFFU);
+    io_out8(dev->base + REG_LBA_LOW, (node->LBA) & 0xFFU);
+
+    // command
+    if (node->command == DISK_READ) {
+        io_out8(dev->base + REG_COMMAND, ext ? ATA_CMD_READ_EXT : ATA_CMD_READ);
+    } else if (node->command == DISK_WRITE) {
+        io_out8(dev->base + REG_COMMAND, ext ? ATA_CMD_WRITE_EXT : ATA_CMD_WRITE);
+        // TODO write
+    } else {
+        io_out8(dev->base + REG_COMMAND, ATA_CMD_IDENTIFY);
+    }
+}
+
+void destroy_request() {
+    kfree(in_using);
+    in_using = NULL;
+    if (req_cnt)
+        exec_next_cmd();
+}
+
+void read_handler(irq_nr_t nr) {
+    struct block_buffer_node *node = in_using;
+    int8_t status = io_in8(node->dev->base + REG_STATUS);
+    print_color(YELLOW, BLACK, "status=%#04x\n", status);
+    port_insw(node->dev->base + REG_DATA, node->buffer, 256 * node->sec_cnt);
+    destroy_request();
+}
+
+void write_handler(irq_nr_t nr) {
+    int8_t status = io_in8(in_using->dev->base + REG_STATUS);
+    print_color(YELLOW, BLACK, "status=%#04x\n", status);
+    destroy_request();
+}
+
+void identify_handler(irq_nr_t nr) {
+    int8_t status = io_in8(in_using->dev->base + REG_STATUS);
+    print_color(YELLOW, BLACK, "status=%#04x\n", status);
+    port_insw(in_using->dev->base + REG_DATA, in_using->buffer, 256);
+    destroy_request();
+}
+
+void other_handler(irq_nr_t nr) {
+    destroy_request();
+}
 
 void disk_handler(irq_nr_t irq_nr, regs_t *regs) {
-    while (io_in8(PORT_DISK1_STATUS_AND_CMD) & DISK_STATUS_BSY) {
-        pause();
-    }
-
-    uint16_t data[256];
-    port_insw(PORT_DISK1_DATA, &data, 256);
-    for (unsigned int i = 0; i < 256; i++) {
-        if ((i & 7U) == 0)
-            print_color(YELLOW, BLACK, "[%03d] ", i);
-        print_color(WHITE, BLACK, "%04x ", data[i]);
-        if ((i & 15U) == 15)
-            println("");
-    }
-    uint16_t sec_cnt = (data[61] << 16U) + data[60];
-    print_color(YELLOW, BLACK, "Hard Disk sector count: %d, size: %dKB", sec_cnt, sec_cnt >> 1U);
+    in_using->handler(irq_nr);
 }
 
 void disk_init() {
@@ -49,20 +126,49 @@ void disk_init() {
 
     register_irq(SLAVE_DISK_IRQ_NR, "slave disk", &disk_ctl, &entry, &disk_handler);
 
+    entry.intr_vector = MASTER_DISK_IRQ_NR;
+    register_irq(MASTER_DISK_IRQ_NR, "master disk", &disk_ctl, &entry, &disk_handler);
+
+    list_init(&block_request.list);
+
     // enable
-    io_out8(PORT_DISK1_STATUS_AND_CTL, 0);
+    io_out8(PORT_DISK0_CTRL, 0);
+    io_out8(PORT_DISK1_CTRL, 0);
+}
 
-    io_out8(PORT_DISK1_ERROR, 0);
-    io_out8(PORT_DISK1_SEC_CNT, 0);
-    io_out8(PORT_DISK1_SEC_NUM, 0);
-    io_out8(PORT_DISK1_CYL_LOW, 0);
-    io_out8(PORT_DISK1_CYL_HIGH, 0);
-    io_out8(PORT_DISK1_DRV_AND_HEAD, 0xE0);
-    if (io_in8(PORT_DISK1_STATUS_AND_CMD) == 0) {
-        print_color(RED, BLACK, "Drive does not exist\n");
-        return;
+struct block_buffer_node *
+make_request(struct ide_device *dev, uint8_t command, uint64_t lba, uint64_t count, void *buffer) {
+    struct block_buffer_node *node = kmalloc(sizeof(struct block_buffer_node));
+    list_init(&node->list);
+    switch (command) {
+        case DISK_READ:
+            node->command = DISK_READ;
+            node->handler = read_handler;
+            break;
+        case DISK_WRITE:
+            node->command = DISK_WRITE;
+            node->handler = write_handler;
+            break;
+        case DISK_IDENTIFY:
+            node->command = DISK_IDENTIFY;
+            node->handler = identify_handler;
+            break;
+        default:
+            node->command = 0;
+            node->handler = other_handler;
+            break;
     }
+    node->dev = dev;
+    node->LBA = lba;
+    node->sec_cnt = count;
+    node->buffer = buffer;
+    return node;
+}
 
-    // identify command
-    io_out8(PORT_DISK1_STATUS_AND_CMD, CMD_DISK_IDENTIFY);
+void submit(struct block_buffer_node *node) {
+    list_add_to_before(&block_request.list, &node->list);
+    req_cnt++;
+    if (in_using == NULL) {
+        exec_next_cmd();
+    }
 }
--- src/kernel/driver/disk.h
+++ src/kernel/driver/disk.h
@@ -6,68 +6,106 @@
 #ifndef _DISK_H
 #define _DISK_H
 
+#include "../lib/defs.h"
+#include "../trap/intr.h"
+#include "../lib/list.h"
+#include "../lib/x86.h"
+
 #define MASTER_DISK_IRQ_NR  0x2D
 #define SLAVE_DISK_IRQ_NR   0x2F
 
 /**
-CHS mode: Cylinder,Head, Sector
-LBA mode: Logical Block Address
-
 I/O PORT: 0x1F6 0x176
-bit7: always 1
-bit6: mode. 0 CHS, 1 LBA
-bit5: always 1
-bit4: drive. 0 master, 1 slave
-bit3-0: head number or LBA27_24
 */
 
 /**
  * Primary/Master DISK: 0x1F0 ~ 0x1F7
  */
-#define PORT_DISK0_DATA             0x1F0U
-#define PORT_DISK0_ERROR            0x1F1U
-#define PORT_DISK0_SEC_CNT          0x1F2U
-#define PORT_DISK0_SEC_NUM          0x1F3U
-#define PORT_DISK0_CYL_LOW          0x1F4U
-#define PORT_DISK0_CYL_HIGH         0x1F5U
-#define PORT_DISK0_DRV_AND_HEAD     0x1F6U
-#define PORT_DISK0_STATUS_AND_CMD   0x1F7U
-
-#define PORT_DISK0_STATUS_AND_CTL   0x3F6
-
+#define PORT_DISK0_BASE         0x1F0U
+#define PORT_DISK0_CTRL         0x3F6U
 /**
  * Secondary/Slave DISK: 0x170 ~ 0x177
  */
-#define PORT_DISK1_DATA             0x170U
-#define PORT_DISK1_ERROR            0x171U
-#define PORT_DISK1_SEC_CNT          0x172U
-#define PORT_DISK1_SEC_NUM          0x173U
-#define PORT_DISK1_CYL_LOW          0x174U
-#define PORT_DISK1_CYL_HIGH         0x175U
-#define PORT_DISK1_DRV_AND_HEAD     0x176U
-#define PORT_DISK1_STATUS_AND_CMD   0x177U
-
-#define PORT_DISK1_STATUS_AND_CTL   0x376
+#define PORT_DISK1_BASE         0x170U
+#define PORT_DISK1_CTRL         0x376U
 
 /**
- * COMMAND
+ * Register offset
  */
-#define CMD_DISK_IDENTIFY   0xEC
-#define CMD_READ_SECTORS    0x20
-#define CMD_WRITE_SECTORS   0x30
+#define REG_DATA        0
+#define REG_ERROR       1
+#define REG_SEC_CNT     2
+#define REG_LBA_LOW     3
+#define REG_LBA_MED     4
+#define REG_LBA_HIGH    5
+/*
+bit 7: always 1
+bit 6: mode. 0 CHS (Cylinder,Head, Sector), 1 LBA (Logical Block Address)
+bit 5: always 1
+bit 4: drive. 0 master, 1 slave
+bit3-0: head number or LBA27_24
+*/
+#define REG_DEV_SEL     6
+#define REG_STATUS      7
+#define REG_COMMAND     7
+
+#define REG_ALT_STATUS  0
+#define REG_DRV_ADDR    1
 
 /**
- * Status
+ * Command value of Command Register
  */
-#define DISK_STATUS_ERR     (1U<<0U)
-#define DISK_STATUS_IDX     (1U<<1U)
-#define DISK_STATUS_CORR    (1U<<2U)
-#define DISK_STATUS_DRQ     (1U<<3U)
-#define DISK_STATUS_SRV     (1U<<4U)
-#define DISK_STATUS_DF      (1U<<5U)
-#define DISK_STATUS_RDY     (1U<<6U)
-#define DISK_STATUS_BSY     (1U<<7U)
+#define ATA_CMD_IDENTIFY    0xEC
+#define ATA_CMD_READ        0x20 // 28 Bit LBA
+#define ATA_CMD_READ_EXT    0x24 // 48 Bit LBA
+#define ATA_CMD_WRITE       0x30 // 28 Bit LBA
+#define ATA_CMD_WRITE_EXT   0x34 // 48 Bit LBA
+#define ATA_CMD_FLUSH       0xE7
+#define ATA_CMD_FLUSH_EXT   0xEA
+
+/**
+ * Status value of Status Register
+ */
+#define STATUS_ERR     (1U<<0U)
+#define STATUS_IDX     (1U<<1U)
+#define STATUS_CORR    (1U<<2U)
+#define STATUS_DRQ     (1U<<3U)
+#define STATUS_SRV     (1U<<4U)
+#define STATUS_DF      (1U<<5U)
+#define STATUS_RDY     (1U<<6U)
+#define STATUS_BSY     (1U<<7U)
+
+#define DISK_READ       0
+#define DISK_WRITE      1
+#define DISK_IDENTIFY   2
+
+struct ide_device {
+    uint16_t base;
+    uint16_t ctrl; // alt status and control
+    uint8_t drive; // 0 master drive, 1 slave drive
+};
+
+struct block_buffer_node {
+    list_t list;
+    void *buffer;
+    uint8_t command;
+    uint16_t sec_cnt; // Sector Count
+    uint64_t LBA; // Logical Block Address
+    struct ide_device *dev;
+
+    void (*handler)(irq_nr_t nr);
+};
+
+static inline struct block_buffer_node *block_next(struct block_buffer_node *block) {
+    return container_of(list_next(&block->list),
+                        struct block_buffer_node, list);
+}
 
 void disk_init();
+
+struct block_buffer_node *
+make_request(struct ide_device *dev, uint8_t command, uint64_t lba, uint64_t count, void *buffer);
+
+void submit(struct block_buffer_node *node);
 
 #endif //_DISK_H
